384b1a4024ab3a29936f340e4c135752721f231d
Juan Ginzo <juanginzo@users.noreply.github.com>
2018-03-23 18:05:27 +0000

    trying to merge

--- juan_eda.py
+++ juan_eda.py
@@ -17,7 +17,7 @@ import pandas as pd
 # 
 # Detailed Review Data for listings in Edinburgh
 
-# In[60]:
+# In[2]:
 
 
 reviews2 = pd.read_csv('airbnb_data/reviews 2.csv')
@@ -29,7 +29,7 @@ reviews2.head(3)
 # 
 # GeoJSON file of neighbourhoods of the city.
 
-# In[46]:
+# In[3]:
 
 
 neighbourhoods_geo_json = pd.read_json('airbnb_data/neighbourhoods.geojson')
@@ -41,7 +41,7 @@ neighbourhoods_geo_json.head(3) # neighbourhood_group is all NaN
 # 
 # Neighbourhood list for geo filter. Sourced from city or open source GIS files.
 
-# In[42]:
+# In[4]:
 
 
 neighbourhoods = pd.read_csv('airbnb_data/neighbourhoods.csv')
@@ -53,7 +53,7 @@ neighbourhoods.head(3) # neighbourhood_group is all NaN
 # 
 # Detailed Calendar Data for listings in Edinburgh
 
-# In[37]:
+# In[5]:
 
 
 calendar = pd.read_csv('airbnb_data/calendar.csv')
@@ -65,7 +65,7 @@ calendar.head(3)
 # 
 # Detailed Listings data for Edinburgh
 
-# In[26]:
+# In[6]:
 
 
 listings2 = pd.read_csv('airbnb_data/listings 2.csv')
@@ -73,20 +73,36 @@ print(listings2.shape)
 listings2.head(1)
 
 
-# In[61]:
+# In[19]:
+
+
+listings2['zipcode'].head()
+
+
+# In[23]:
+
+
+count = 0
+for row in listings2['zipcode']:
+    if len(str(row).split()) == 2:
+        count += 1
+print(count)
+
+
+# In[7]:
 
 
 for col in listings2.columns:
     print(col)
 
 
-# In[104]:
+# In[8]:
 
 
 listings2['neighbourhood'].value_counts()
 
 
-# In[91]:
+# In[9]:
 
 
 listings2['zipcode'].map(lambda x: str(x).split(' ')[0]).value_counts()[:3]
@@ -94,7 +110,7 @@ listings2['zipcode'].map(lambda x: str(x).split(' ')[0]).value_counts()[:3]
 
 # ## EDA
 
-# In[87]:
+# In[10]:
 
 
 df = pd.read_csv('simd2016_withinds.csv')
@@ -102,27 +118,67 @@ deprivation = df[df['Council_area'] == 'City_of_Edinburgh']
 deprivation.head()
 
 
-# In[105]:
-
-
-deprivation['Intermediate_Zone'].value_counts()
-
-
-# In[117]:
+# In[11]:
 
 
 deprivation.shape
 
 
-# In[116]:
-
-
-deprivation.head()
-
-
-# In[118]:
+# In[12]:
 
 
 simd16 = pd.read_csv('simd16.csv')
 simd16.head()
+
+
+# In[13]:
+
+
+simd16.shape
+
+
+# ## main deprivation df
+
+# In[14]:
+
+
+deprivation_all = pd.merge(simd16, deprivation, left_on='DZ', right_on='Data_Zone', how='right')
+print(deprivation_all.shape)
+deprivation_all.head()
+
+
+# Merging deprivation with listings
+
+# In[35]:
+
+
+df_all = pd.merge(listings2, deprivation_all, left_on='zipcode', right_on='Postcode', how='left')
+print(listings2.shape)
+print(deprivation_all.shape)
+print(df_all.shape)
+df_all[['zipcode', 'Postcode']].head(10)
+
+
+# In[47]:
+
+
+len(listings2['id'].value_counts())
+
+
+# In[49]:
+
+
+len(df_all['id'].value_counts())
+
+
+# In[44]:
+
+
+df_all['id'].isnull().sum()
+
+
+# In[52]:
+
+
+df_all.drop_duplicates()
 
